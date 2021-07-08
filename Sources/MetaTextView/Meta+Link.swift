9021b330dd72898583f62ee7f4c98768d72e7654
CMK <cirno.mainasuk@gmail.com>
2021-07-08 16:50:47 +0800

    feat: add userInfo for meta link

--- Sources/MetaTextView/Meta+Link.swift
+++ Sources/MetaTextView/Meta+Link.swift
@@ -21,14 +21,23 @@ extension Meta {
 
         guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
               let parameters = components.queryItems else { return nil }
+        let userInfo: [AnyHashable: Any] = {
+            var dict: [AnyHashable: Any] = [:]
+            for queryItem in parameters {
+                guard let stringValue = queryItem.value,
+                      let value = String(base64Encoded: stringValue) else { continue }
+                dict[queryItem.name] = value
+            }
+            return dict
+        }()
 
         if let hashtag = parameters.first(where: { $0.name == "hashtag" }), let encoded = hashtag.value, let value = String(base64Encoded: encoded) {
-            self = .hashtag("#\(value)", hashtag: value, userInfo: nil)
+            self = .hashtag("#\(value)", hashtag: value, userInfo: userInfo)
             return
         }
 
         if let mention = parameters.first(where: { $0.name == "mention" }), let encoded = mention.value, let value = String(base64Encoded: encoded) {
-            self = .mention("@\(mention)", mention: value, userInfo: nil)
+            self = .mention("@\(mention)", mention: value, userInfo: userInfo)
             return
         }
         
@@ -39,13 +48,30 @@ extension Meta {
         switch self {
         case .url(_, _, let url, _):
             return URL(string: url)
-        case .hashtag(_, let hashtag, _):
-            return URL(string: "\(Meta.metaLinkScheme)://meta?hashtag=\(hashtag.base64Encoded)")
-        case .mention(_, let mention, _):
-            return URL(string: "\(Meta.metaLinkScheme)://meta?mention=\(mention.base64Encoded)")
+        case .hashtag(_, let hashtag, let userInfo):
+            let components = Meta.createURLComponents(key: "hashtag", value: hashtag, userInfo: userInfo)
+            return components.url
+        case .mention(_, let mention, let userInfo):
+            let components = Meta.createURLComponents(key: "mention", value: mention, userInfo: userInfo)
+            return components.url
         default:
             return nil
         }
+    }
+
+    static func createURLComponents(key: String, value: String, userInfo: [AnyHashable: Any]?) -> URLComponents {
+        var components = URLComponents(string: "\(Meta.metaLinkScheme)://meta")!
+
+        var items: [URLQueryItem] = [URLQueryItem(name: key, value: value.base64Encoded)]
+        for (key, value) in userInfo ?? [:] {
+            guard let stringKey = key as? String,
+                  let stringValue = value as? String else { continue }
+            let item = URLQueryItem(name: stringKey, value: stringValue.base64Encoded)
+            items.append(item)
+        }
+        components.queryItems = items
+
+        return components
     }
 
 }
