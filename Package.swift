f931ef728609fb8b8c55153f49be231622b8a473
CMK <cirno.mainasuk@gmail.com>
2021-09-06 14:13:52 +0800

    chore: update package configuration

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MetaTextKit",
-            targets: ["MetaTextKit"]),
-        .library(
             name: "MetaTextArea",
             targets: ["MetaTextArea"]),
+        .library(
+            name: "MetaTextKit",
+            targets: ["MetaTextKit"]),
         .library(
             name: "MastodonMeta",
             targets: ["MastodonMeta"]),
@@ -31,10 +31,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MetaTextKit",
-            dependencies: ["Meta"]),
-        .target(
             name: "MetaTextArea",
+            dependencies: ["Meta", "MetaTextKit"]),
+        .target(
+            name: "MetaTextKit",
             dependencies: ["Meta"]),
         .target(
             name: "MastodonMeta",
