e35b28b913762c7a9a6d1e36561a558376962a52
CMK <cirno.mainasuk@gmail.com>
2023-01-09 17:58:02 +0800

    fix: non-normalized emoji domain can not be bypass to AttributedString issue

--- Sources/Meta/Meta+Entity.swift
+++ Sources/Meta/Meta+Entity.swift
@@ -22,7 +22,9 @@ extension Meta {
 extension Meta.Entity {
     public var primaryText: String {
         switch self.meta {
-        case .url(let text, _, _, _):           return text
+        case .url(let text, _, _, _):
+            // fix emoji not accepted by AttributedString issue
+            return URLComponents(string: text)?.url?.absoluteString ?? text
         case .emoji(let text, _, _, _):         return text
         case .hashtag(let text, _, _):          return text
         case .cashtag(let text, _, _):          return text
